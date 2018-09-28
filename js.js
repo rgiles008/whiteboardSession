e986128fa72d28f26568f63aee8a548f5103cd23
Robert Giles <robert.giles888@gmail.com>
2018-09-28 14:54:28 -0600

    finishing up the questions

--- js.js
+++ js.js
@@ -26,7 +26,7 @@ function reverseString(str) {
   // return revString;
 
   let revString = '';
-  str.split('').forEach(char =>revString = char + revString);
+  str.split('').forEach(char => revString = char + revString);
   return revString;
 }
 
@@ -58,24 +58,75 @@ function reverseInt(int) {
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) {
+  // const strArr = str.toLowerCase().split(' ');
+  // for(let i = 0; i < strArr.length; i++){
+  //   strArr[i] = strArr[i]
+  //                 .subString(0, 1)
+  //                 .toUpperCase() + strArr[i]
+  //                 .subString(1);
+  // }
+  // return strArr.join(' ');
+
+  // Refactored option with es6
+  // return str
+  //   .toLowerCase()
+  //   .split(' ')
+  //   .map(word => word[0].toUpperCase() + word.substr(1)) 
+  //   .join(' ');
+
+  // Using regular expressions
+  return str.replace(/\b[a-z]/gi, (char) => char.toUpperCase());
+} 
 
 
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+function maxCharacter(str) {
+  const charMap = {};
+  let maxNum = 0;
+  let maxChar = '';
+
+  str.split('').forEach(function(char){
+    if(charMap[char]){
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+
+  for(let char in charMap){
+    if(charMap[char] > maxNum){
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+  for(let i = 0; i <= 100; i++){
+    if(i % 15 === 0){
+      console.log('fizzBuzz');
+    } else if(i % 3 === 0){
+      console.log('fizz');
+    } else if(i % 5 === 0){
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 
 
 // Call Function
-const output = isPalindrome('racecar');
+const output = fizzBuzz();
 
 console.log(output);
